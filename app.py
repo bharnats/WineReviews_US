55762761bec41343b0470eacc14a537c3fb36672
Bharathy <bharathy_nats@yahoo.com>
2018-03-03 09:40:03 -0600

    Moved database code under main

--- app.py
+++ app.py
@@ -14,17 +14,6 @@ app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-dbfile = os.path.join('raw_data/wine_reviews.sqlite')
-engine = create_engine(f"sqlite:///{dbfile}")
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-# Save references to each table
-wine_reviews = Base.classes.reviews 
-# Create our session (link) from Python to the Database
-session = Session(engine)
-
 
 @app.route("/")
 def index():
@@ -112,6 +101,16 @@ def stateData(state):
 
 
 if __name__ == "__main__":
+    dbfile = os.path.join('raw_data/wine_reviews.sqlite')
+    engine = create_engine(f"sqlite:///{dbfile}")
+    # reflect an existing database into a new model
+    Base = automap_base()
+    # reflect the tables
+    Base.prepare(engine, reflect=True)
+    # Save references to each table
+    wine_reviews = Base.classes.reviews 
+    # Create our session (link) from Python to the Database
+    session = Session(engine)
     app.run(debug=True)
     
 
