2dc7a683a3d5d6a2345c5686fee86d19eacc2876
Carlos Guevara <guevara.t.carlos@gmail.com>
2018-03-08 21:39:40 -0600

    updated files

--- static/js/cluster.js
+++ static/js/cluster.js
@@ -1,43 +1,59 @@
+function init() {
+  buildMap();
+};
 
+init();
 
-// Creating map object
-var myMap = L.map("map", {
-  center: [37.52, -101.67],
-  zoom: 5
-});
-// Mapbox API
-var mapbox = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1IjoiYmhhcmF0aHktbmF0cyIsImEiOiJjamRoa2RrdHcwem12MndvMXpmM2Z5YnM1In0.4zch_H0WTdg4af5YTLvImg." +
-    "T6YbdDixkOBWH_k9GbS8JQ";
+function buildMap() {
 
-// Adding tile layer to the map
-L.tileLayer(mapbox).addTo(myMap)
+    // Creating map object
+  var myMap = L.map("map", {
+    center: [38.32, -95.67],
+    zoom: 4.1,
+    minZoom: 3,
+    maxZoom: 10,
+  });
+  // Mapbox API
+  var mapbox = "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+      "access_token=pk.eyJ1IjoiYmhhcmF0aHktbmF0cyIsImEiOiJjamRoa2RrdHcwem12MndvMXpmM2Z5YnM1In0.4zch_H0WTdg4af5YTLvImg." +
+      "T6YbdDixkOBWH_k9GbS8JQ";
 
-// API Query URL
-var url = '/cluster_data';
+  console.log(mapbox);
 
-// Grabbing the data with d3..
-d3.json(url, function(response) {
+  // Adding tile layer to the map
+  L.tileLayer(mapbox).addTo(myMap)
 
-  // Creating a new marker cluster group
-  var markers = L.markerClusterGroup();
+  // API Query URL
+  var url = '/cluster_data';
+  console.log(url);
 
-  // Loop through our data...
-  for (var i = 0; i < response.length; i++) {
-    // set the data location property to a variable
-    var location= response[i].coordinates;
-console.log(location);
-    // If the data has a location property...
-    if (location) {
+  // Grabbing the data with d3..
+  Plotly.d3.json(url, function(error, response) {
+    if (error) return console.warn(error);
 
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location[0], location[1]])
-        .bindPopup(("<h3># grape variety: " +response[i].variety_count +
-      "</h3><hr><h3> region: " + response[i].region + "</h3>") ));
+    // Creating a new marker cluster group
+    var markers = L.markerClusterGroup();
+
+    // Loop through our data...
+    for (var i = 0; i < response.length; i++) {
+      // set the data location property to a variable
+      var location= response[i].coordinates;
+  console.log(location);
+      // If the data has a location property...
+      if (location) {
+
+        // Add a new marker to the cluster group and bind a pop-up
+        markers.addLayer(L.marker([location[0], location[1]])
+          .bindPopup(("<h3># grape variety: " +response[i].variety_count +
+        "</h3><hr><h3> region: " + response[i].region + "</h3>") ));
+      }
+
     }
+  // Add our marker cluster layer to the map
+    myMap.addLayer(markers);
 
-  }
-// Add our marker cluster layer to the map
-  myMap.addLayer(markers);
+  });
 
-});
+}
+
+
--- static/js/data.js
+++ static/js/data.js
@@ -1,54 +1,3 @@
-// // select the dom elements and set it to variables(using querySelector) 
-// var $tbody = document.querySelector("tbody");
-// var $searchBtn = document.getElementById("search");
-// var $stateInput = document.querySelector("#state");
-
-
-// // $searchBtn.addEventListener("click", handleSearchButtonClick);
-
-
-// function renderTable() {
-
-//     // var url = "/data";
-//     Plotly.d3.json('/tabledata', function (error, response) {
-//         var input = $stateInput.value.trim().toLowerCase()
-//         var wineReviews = response.filter(function(review) {
-//             var reviewState = review.province.toLowerCase();
-        
-//             // If true, add the review to the wineReviews, otherwise don't add it to wineReviews
-//             return reviewState === input;
-//           });
-
-//         // var wineReviews = response;
-//         console.log(wineReviews)
-//         $tbody.innerHTML = "";
-//         for (var i = 0; i < wineReviews.length; i++) {
-//             // Get get the current review object and its fields
-//             var review = wineReviews[i];
-//             var fields = Object.keys(review);
-//             // Create a new row in the tbody, set the index to be i + startingIndex
-//             var $row = $tbody.insertRow(i);
-//             for (var j = 0; j < fields.length; j++) {
-//                 // For every field in the review object, create a new cell at set its inner text to be the current value at the current review's field
-//                 var field = fields[j];
-//                 var $cell = $row.insertCell(j);
-//                 $cell.innerText = review[field];
-//             }
-//         }
-//     })
-// }
-
-// function handleSearchButtonClick($stateInput) {
-//     // Format the user's search by removing leading and trailing whitespace, lowercase the string
-//     var filterState = $stateInput.value.trim().toLowerCase();
-  
-//     // Set wineReviews to an array of all reviewes whose "state" matches the filter
-//     renderTable();
-// }
-
-//////////////////////////////////////////////////////////////////////////////////////
-/////////// /////////////// ////New Table//// //////////////////// ///////////////////
-////////////////////////////////////////////////////////////////////////////////////// 
 function init() {
     getData();
 };
@@ -63,16 +12,6 @@ function getData() {
         buildTable(data);
     });
 };
-
-// function buildGraphdivs(data) {
-//     // d3.select("#mapid").remove();
-//     d3.select("#rawData").remove();
-//     // d3.select("#mapContainer").html('<div id="mapid" class="map" style="width: 100%; height: 800px; border: 3px solid #AAA;"></div>');
-//     d3.select("#insertTable").html('<table id="rawData" class="display" style="width: 90%; height: 800px; padding: 4px solid #AAA;"></table>');
-//     // buildMap(data);
-//     buildTable(data);
-
-// };
 
 function buildTable(data) {
     var tableArray =[];
