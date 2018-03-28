9dd89beffe68de0bbdb3a7a9ac70db95e4eb97a3
Bharathy <bharathy_nats@yahoo.com>
2018-03-27 21:43:36 -0500

    make better

---
+++ pgenv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export PGHOST=ec2-54-243-31-34.compute-1.amazonaws.com \
+export PGPORT=5432 \
+export PGDATABASE=d3h3geasr9gqhd \
+export PGUSER=zpwindpjwximhr \
+export PGPASSWORD=4c4745b4a4757f38935dba160e0568fe66107784d89ce5bfeae9e379a97b0d78
